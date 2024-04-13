3efb0e82e026b2d1968d1eee6e512e27a06029b5
partycrusher546 <muthonidennis546@gmail.com>
2024-04-14 01:09:46 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskDescription = document.getElementById("new-task-description").value;
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+    taskList.appendChild(taskItem);
+    form.reset();
+  });
 });
